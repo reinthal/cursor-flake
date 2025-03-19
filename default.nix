0c41866c6874fe7cc95f4dc5abc49997147019d1
Alexander Reinthal <email@reinthal.me>
2025-03-19 22:37:47 +0100

    refactor: architecture to var

--- default.nix
+++ default.nix
@@ -1,47 +1,51 @@
-{ pkgs, appimageTools, ... }:
-
-let
+{
+  pkgs,
+  appimageTools,
+  ...
+}: let
   pname = "cursor";
   version = "0.40.4";
   appKey = "230313mzl4w4u92";
   buildKey = "2409052yfcjagw2";
+  architecture = "x86_64";
 
   src = pkgs.fetchurl {
-    url = "https://download.todesktop.com/${appKey}/cursor-${version}-build-${buildKey}-x86_64.AppImage";
+    url = "https://download.todesktop.com/${appKey}/cursor-${version}-build-${buildKey}-${architecture}.AppImage";
     hash = "sha256-ZURE8UoLPw+Qo1e4xuwXgc+JSwGrgb/6nfIGXMacmSg=";
   };
-  appimageContents = appimageTools.extract { inherit pname version src; };
+  appimageContents = appimageTools.extract {inherit pname version src;};
 in
-appimageTools.wrapType2 {
-  inherit pname version src;
+  appimageTools.wrapType2 {
+    inherit pname version src;
 
-  extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-quiet 'Exec=AppRun' 'Exec=${pname}'
-    cp -r ${appimageContents}/usr/share/icons $out/share
+    extraInstallCommands = ''
+      install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace-quiet 'Exec=AppRun' 'Exec=${pname}'
+      cp -r ${appimageContents}/usr/share/icons $out/share
 
-    if [ -e ${appimageContents}/AppRun ]; then
-      install -m 755 -D ${appimageContents}/AppRun $out/bin/${pname}-${version}
-      if [ ! -L $out/bin/${pname} ]; then
-        ln -s $out/bin/${pname}-${version} $out/bin/${pname}
+      if [ -e ${appimageContents}/AppRun ]; then
+        install -m 755 -D ${appimageContents}/AppRun $out/bin/${pname}-${version}
+        if [ ! -L $out/bin/${pname} ]; then
+          ln -s $out/bin/${pname}-${version} $out/bin/${pname}
+        fi
+      else
+        echo "Error: Binary not found in extracted AppImage contents."
+        exit 1
       fi
-    else
-      echo "Error: Binary not found in extracted AppImage contents."
-      exit 1
-    fi
-  '';
+    '';
 
-  extraBwrapArgs = [
-    "--bind-try /etc/nixos/ /etc/nixos/"
-  ];
+    extraBwrapArgs = [
+      "--bind-try /etc/nixos/ /etc/nixos/"
+    ];
 
-  dieWithParent = false;
+    dieWithParent = false;
 
-  extraPkgs = pkgs: with pkgs; [
-    unzip
-    autoPatchelfHook
-    asar
-    (buildPackages.wrapGAppsHook.override { inherit (buildPackages) makeWrapper; })
-  ];
-}
+    extraPkgs = pkgs:
+      with pkgs; [
+        unzip
+        autoPatchelfHook
+        asar
+        (buildPackages.wrapGAppsHook.override {inherit (buildPackages) makeWrapper;})
+      ];
+  }
